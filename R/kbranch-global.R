26f70e4b451cdede6edad2fe8d36ce3d85b01583
nchlis <nchlis@isc.tuc.gr>
2016-11-17 12:04:15 +0100

    fixed double package help file

--- R/kbranch-global.R
+++ R/kbranch-global.R
@@ -1,11 +1,3 @@
-#' Clustering on single cell trajectories.
-#'
-#' The \pkg{kbranch} package provides two main functions
-#' \itemize{
-#'    \item \code{\link{kbranch.global}}
-#'    \item \code{\link{kbranch.local}}
-#' }
-"_PACKAGE"
 #'
 #'Clustering on K-Branches
 #'
